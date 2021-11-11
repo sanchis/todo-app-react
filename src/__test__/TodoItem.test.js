a51110a0d0cbceac22bc6d694327b94f9e1d1997
Adrián Sanchis <adrian@devaway.es>
2021-11-11 09:49:02 +0100

    add beforeach TodoItem test

--- src/__test__/TodoItem.test.js
+++ src/__test__/TodoItem.test.js
@@ -3,98 +3,69 @@ import TodoItem from '../TodoList/components/TodoItem'
 import { TodosContext } from '../TodoList/context/TodosContext'
 
 describe('Test TodoItem', () => {
-  test('Render TodoItem', () => {
-    const item = {
-      id: '1',
-      name: 'This is my new todo item',
-      fav: false,
-      checked: false,
-      createdDate: new Date()
-    }
-    const component = render(<TodoItem item={item} />)
+  const item = {
+    id: '1',
+    name: 'This is my new todo item',
+    fav: false,
+    checked: false,
+    createdDate: new Date()
+  }
+  let component
+  const handleClickCheck = jest.fn()
+  const handleClickFav = jest.fn()
+  const handleClickDel = jest.fn()
 
+  beforeEach(() => {
+    component = render(
+      <TodosContext.Provider value={{
+        updateTodo: jest.fn(),
+        deleteTodo: jest.fn()
+      }}
+      >
+        <TodoItem
+          item={item}
+          onChangeCheck={handleClickCheck}
+          onChangeFav={handleClickFav}
+          onDelete={handleClickDel}
+        />
+      </TodosContext.Provider>
+    )
+  })
+
+  test('Render TodoItem', () => {
     expect(component.getByTestId('name').textContent).toEqual(item.name)
     expect(component.getByTestId('checkButton').checked).toEqual(item.checked)
     expect(component.getByTestId('favButton').value).toEqual(item.fav.toString())
   })
 
   test('TodoItem should be checked', () => {
-    const item = {
-      id: '1',
-      name: 'This is my new todo item',
-      fav: false,
-      checked: false,
-      createdDate: new Date()
-    }
-
     const expectedItem = { ...item, checked: !item.checked }
-    const handleClick = jest.fn()
 
-    const component = render(
-      <TodosContext.Provider value={{
-        updateTodo: jest.fn()
-      }}
-      >
-        <TodoItem item={item} onChangeCheck={handleClick} />
-      </TodosContext.Provider>
-    )
     const checkButton = component.getByTestId('checkButton')
 
     expect(checkButton.checked).toEqual(item.checked)
     fireEvent.click(checkButton)
 
-    expect(handleClick).toHaveBeenCalledTimes(1)
-    expect(handleClick).toHaveBeenCalledWith(expectedItem)
+    expect(handleClickCheck).toHaveBeenCalledTimes(1)
+    expect(handleClickCheck).toHaveBeenCalledWith(expectedItem)
   })
 
   test('TodoItem should be favorite', () => {
-    const item = {
-      id: '1',
-      name: 'This is my new todo item',
-      fav: false,
-      checked: false,
-      createdDate: new Date()
-    }
     const expectedItem = {
       ...item,
       fav: true
     }
-    const handleClick = jest.fn()
-    const component = render(
-      <TodosContext.Provider value={{
-        updateTodo: jest.fn()
-      }}
-      >
-        <TodoItem item={item} onChangeFav={handleClick} />
-      </TodosContext.Provider>
-    )
     const favButton = component.getByTestId('favButton')
 
     fireEvent.click(favButton)
-    expect(handleClick).toHaveBeenCalledTimes(1)
-    expect(handleClick).toBeCalledWith(expectedItem)
+    expect(handleClickFav).toHaveBeenCalledTimes(1)
+    expect(handleClickFav).toBeCalledWith(expectedItem)
   })
 
   test('TodoItem should be deleted', () => {
-    const item = {
-      id: '1',
-      name: 'This is my new todo item',
-      fav: false,
-      checked: false,
-      createdDate: new Date()
-    }
-    const handleClick = jest.fn()
-    const component = render(
-      <TodosContext.Provider value={{
-        deleteTodo: jest.fn()
-      }}
-      >
-        <TodoItem item={item} onDelete={handleClick} />
-      </TodosContext.Provider>
-    )
     const delButton = component.getByTestId('deleteButton')
 
     fireEvent.click(delButton)
-    expect(handleClick).toHaveBeenCalledTimes(1)
+    expect(handleClickDel).toHaveBeenCalledTimes(1)
   })
 })
